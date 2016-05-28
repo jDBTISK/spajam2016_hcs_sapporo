6d7e4db48d743374b50bb5bf265673bc02646264
Ryosuke10 <xbreck911@gmail.com>
2016-05-28 17:36:43 +0900

    main

--- app/src/main/java/com/example/jdb/bocciinterviewapp/MainActivity.java
+++ app/src/main/java/com/example/jdb/bocciinterviewapp/MainActivity.java
@@ -1,14 +1,49 @@
 package com.example.jdb.bocciinterviewapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //tesuto
+        Button button = (Button)findViewById(R.id.button);
+        Button button2 = (Button)findViewById(R.id.button2);
+        Button button3 = (Button)findViewById(R.id.button3);
+        Button button4 = (Button)findViewById(R.id.button4);
+
+        button.setOnClickListener(this);
+        button2.setOnClickListener(this);
+        button3.setOnClickListener(this);
+        button4.setOnClickListener(this);
+    }
+    @Override
+    public void onClick(View v) {
+        Intent intent;
+        switch (v.getId()) {
+            case R.id.button:
+                intent = new Intent(this, Gesture.class);
+                startActivity(intent);
+                break;
+            case R.id.button2:
+                intent = new Intent(this, Gesture.class);
+                startActivity(intent);
+                break;
+            case R.id.button3:
+                intent = new Intent(this, Gesture.class);
+                startActivity(intent);
+                break;
+            case R.id.button4:
+                intent = new Intent(this, Gesture.class);
+                startActivity(intent);
+                break;
+            default:
+                break;
+        }
     }
 }
