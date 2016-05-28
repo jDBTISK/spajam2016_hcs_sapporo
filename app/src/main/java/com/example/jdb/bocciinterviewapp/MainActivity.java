169848de1257cca23ef3f10e0e1d8f780d2e67f6
ikyasam18 <ikuyasam18@gmail.com>
2016-05-28 20:00:57 +0900

    ko

--- app/src/main/java/com/example/jdb/bocciinterviewapp/MainActivity.java
+++ app/src/main/java/com/example/jdb/bocciinterviewapp/MainActivity.java
@@ -1,27 +1,40 @@
 package com.example.jdb.bocciinterviewapp;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button button = (Button)findViewById(R.id.button);
-        Button button2 = (Button)findViewById(R.id.button2);
-        Button button3 = (Button)findViewById(R.id.button3);
-        Button button4 = (Button)findViewById(R.id.button4);
+        Button button = (Button) findViewById(R.id.button);
+        Button button2 = (Button) findViewById(R.id.button2);
+        Button button3 = (Button) findViewById(R.id.button3);
+        Button button4 = (Button) findViewById(R.id.button4);
 
         button.setOnClickListener(this);
         button2.setOnClickListener(this);
         button3.setOnClickListener(this);
         button4.setOnClickListener(this);
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
+
     @Override
     public void onClick(View v) {
         Intent intent;
@@ -35,7 +48,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 startActivity(intent);
                 break;
             case R.id.button3:
-                intent = new Intent(this, Gesture.class);
+                intent = new Intent(this, Question_and_answer.class);
                 startActivity(intent);
                 break;
             case R.id.button4:
@@ -46,4 +59,5 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 break;
         }
     }
+
 }
---
+++ app/src/main/java/com/example/jdb/bocciinterviewapp/Question_and_answer.java
@@ -0,0 +1,26 @@
+package com.example.jdb.bocciinterviewapp;
+
+import android.app.TabActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TabHost;
+import android.widget.TabHost.TabSpec;
+
+public class Question_and_answer extends TabActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_question_and_answer);
+        TabHost tabHost = getTabHost();
+        TabSpec tab1 = tabHost.newTabSpec("質問リスト");
+        tab1.setIndicator("tab1");
+        tab1.setContent(new Intent(this, QuestionListActivity.class));
+        tabHost.addTab(tab1);
+
+        //TabHost.TabSpec tab2 = tabHost.newTabSpec("プレイリスト");
+        //tab2.setIndicator("tab2");
+        //tab2.setContent(new Intent(this, tab2Activity.class));
+        //tabHost.addTab(tab2);
+    }
+}
