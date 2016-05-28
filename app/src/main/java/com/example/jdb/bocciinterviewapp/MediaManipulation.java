5e312a7607971ee5fa11df8e325f8d6fd357b68c
MuramotoN <muna1017_549@outlook.jp>
2016-05-28 18:54:45 +0900

    Media

---
+++ app/src/main/java/com/example/jdb/bocciinterviewapp/MediaManipulation.java
@@ -0,0 +1,78 @@
+package com.example.jdb.bocciinterviewapp;
+
+import android.media.MediaPlayer;
+import android.media.MediaRecorder;
+import android.os.Environment;
+
+import java.io.File;
+
+public class MediaManipulation {
+
+    private MediaPlayer mediaPlayer;
+    private MediaRecorder mediaRecorder;
+    private String filePath;
+    private File dir, file;
+
+    public MediaManipulation() {
+        mediaPlayer = new MediaPlayer();
+        mediaRecorder = new MediaRecorder();
+
+        dir = new File(Environment.getExternalStorageDirectory(), "BocciMedia");    //保存ディレクトリの指定
+        if(dir.exists() == false) dir.mkdir();  //保存ディレクトリがない場合作成
+    }
+
+    public void recodingStart(String number) {
+        filePath = "bocci" + number + ".3gp";
+        file = new File(dir, filePath);
+
+        //設定
+        mediaRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+        mediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+        mediaRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.DEFAULT);
+        mediaRecorder.setOutputFile(file.getPath());
+
+        //準備
+        try {
+            mediaRecorder.prepare();
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+
+        mediaRecorder.start();
+    }
+
+    public void recodingStop() {
+        try {
+            mediaRecorder.stop();
+            mediaRecorder.reset();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void close() {
+        mediaRecorder.release();
+        mediaPlayer.release();
+    }
+
+    public void playStart(String number) {
+        filePath = "bocci" + number + ".3gp";
+        file = new File(dir, filePath);
+
+        try {
+            mediaPlayer.reset();
+            mediaPlayer.setDataSource(file.getPath());
+            mediaPlayer.prepare();
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+        mediaPlayer.start();
+
+    }
+
+    public void playStop() {
+        mediaPlayer.stop();
+        mediaPlayer.reset();
+    }
+
+}
