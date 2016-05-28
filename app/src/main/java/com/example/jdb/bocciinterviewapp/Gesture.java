a57f45e38c213bde0234d4c5c96092cb04ec4c7d
ikyasam18 <ikuyasam18@gmail.com>
2016-05-28 15:15:17 +0900

    ko

---
+++ app/src/main/java/com/example/jdb/bocciinterviewapp/Gesture.java
@@ -0,0 +1,13 @@
+package com.example.jdb.bocciinterviewapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Gesture extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gesture);
+    }
+}
