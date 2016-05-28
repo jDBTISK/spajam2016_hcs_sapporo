e06fb7b69d4d05a8dd530ed3e98eb75cf23d5dac
ikyasam18 <ikuyasam18@gmail.com>
2016-05-28 15:22:09 +0900

    ko

--- app/src/main/java/com/example/jdb/bocciinterviewapp/Gesture.java
+++ app/src/main/java/com/example/jdb/bocciinterviewapp/Gesture.java
@@ -9,6 +9,6 @@ public class Gesture extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gesture);
-        //kome
+        //kom
     }
 }
