9b385a4a00c3a476e962df09d8a3af2f9055415c
近藤泰介 <jDB@jDB-MacBook-Pro.local>
2016-05-28 19:24:38 +0900

    import Bootstrap

---
+++ app/src/main/java/com/example/jdb/bocciinterviewapp/QuestionListActivity.java
@@ -0,0 +1,18 @@
+package com.example.jdb.bocciinterviewapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+
+public class QuestionListActivity extends AppCompatActivity {
+
+    private BaseAdapter adapter;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_question_list);
+        ListView questionList=(ListView)findViewById(R.id.questionList);
+
+    }
+}
